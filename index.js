441eec1936f1facaa68e82d19a919cbfa0521b4a
George Lugo <stg77713@gmail.com>
2020-03-13 01:58:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -33,7 +33,12 @@ function viewCart() {
 }
 
 function total() {
-  // write your code here
+  // write y
+  total = 0;
+  for(var i = 0; i < cart.length; i++){
+    total += cart[i].itemPrice;
+  }
+  return total;
 }
 
 function removeFromCart(item) {
