d1f9e70de2207fd0ace235825a864ea0f746a5e0
George Lugo <stg77713@gmail.com>
2020-03-13 02:07:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
-  if (cardNumber === 0) {
+  if (typeof(cardNumber) === "undefined") {
     return "Sorry, we don't have a credit card on file for you."
   } else {
     cart = [];
